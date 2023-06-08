c45210efaacaed800cfd02876c34609c7f7cdff6
ulim9707@naver.com <ulim9707@naver.com>
2023-06-08 10:33:13 +0900

    initialize servletProject

--- src/test/java/hello/servlet/domain/member/MemberRepositoryTest.java
+++ src/test/java/hello/servlet/domain/member/MemberRepositoryTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MemberRepositoryTest {
@@ -18,12 +21,28 @@ class MemberRepositoryTest {
     @Test
     void Save() {
         // given
-        Member member =  new Member("hello", 20);
+        Member member = new Member("hello", 20);
         // when
         Member savedMember = memberRepository.save(member);
-
         // then
         Member findMember = memberRepository.findById(savedMember.getId());
+        assertThat(findMember).isEqualTo(savedMember);
+    }
 
+    @Test
+    void findAll() {
+        // given
+        Member member1 = new Member("member1", 20);
+        Member member2 = new Member("member2", 30);
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        // when
+        List<Member> result = memberRepository.findAll();
+
+        // then
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result).contains(member1, member2);
     }
 }
